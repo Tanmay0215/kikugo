44229a2f49f3ad89066b973f1c1b87c6456fdd99
Tanmay0215 <tanmaygupta.0215@gmail.com>
2025-03-11 21:23:46 +0530

    add framer motion and update navbar

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,42 +1,57 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
-import Downloads from '../components/Downloads'
 import dashboard from '../assets/dashboard.png'
 import Footer from '../components/Footer'
 import Features from './Features'
 import kanji from '../assets/JoyoKanji.png'
+import { motion } from 'motion/react'
 
 const Home = () => {
   return (
     <div>
       <Navbar />
-      <div className="flex flex-col justify-center items-center">
-        <div className="absolute z-10 h-100 blur-2xl bg-primary" />
-        <div className="max-w-lg text-4xl md:text-6xl md:text-center text-white font-bold pt-10">
+      <motion.div
+        initial={{ y: 100, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+        transition={{ duration: 1 }}
+        className="flex flex-col justify-center items-center"
+      >
+        <div className="max-w-md text-4xl uppercase md:text-5xl md:text-center text-gray-300 font-bold pt-10">
           The Easiest Way To Learn <br />
-          <span className="text-primary">Japanese</span>
+          <span className="text-primary text-7xl">Japanese</span>
         </div>
         <div className="py-5 max-w-lg text-center text-gray-400">
           Kikugo is a language learning app that helps you learn Japanese in a
           fun and interactive way.
         </div>
-        <button className="bg-white text-black rounded-full px-4 py-2">
-          <a href="/kikugo.exe" download="kikugo.exe">
-            Download Now
-          </a>
-        </button>
-      </div>
-      <div className="flex flex-col items-center justify-center py-10">
-        <h2 className="text-4xl font-bold text-center mb-5">Interactive Dashboard</h2>
+      </motion.div>
+
+      {/* Dashboard */}
+      <motion.div
+        initial={{ x: -100, opacity: 0 }}
+        animate={{ x: 0, opacity: 1 }}
+        transition={{ duration: 1 }}
+        className="flex flex-col items-center justify-center py-10"
+      >
+        <h2 className="text-4xl font-bold text-center mb-5">
+          Interactive Dashboard
+        </h2>
         <div className="max-w-5xl mx-auto rounded-xl py-2 border-2 border-gray-400 shadow-gray-300 shadow-lg">
           <img src={dashboard} alt="Dashboard" />
         </div>
-      </div>
+      </motion.div>
       <Features />
-      <div className="max-w-5xl mx-auto rounded-xl py-2 border-2 m-10 border-gray-400 shadow-gray-300 shadow-lg">
-          <img src={kanji} alt="Kanji" />
+      <div className="flex items-center justify-center gap-5 rounded-xl py-2 m-10">
+        <img src={kanji} alt="Kanji" className="w-2/5" />
+        <div className="w-1/2">
+          <p className="text-3xl font-bold text-primary">KANJI</p>
+          <p className="text-gray-400 text-sm">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Temporibus
+            voluptatem adipisci ducimus aperiam, consequatur odio deleniti
+            suscipit distinctio, nemo sunt animi?
+          </p>
         </div>
-      <Downloads />
+      </div>
       <Footer />
     </div>
   )
