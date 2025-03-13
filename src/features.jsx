8506d0a632b28c8085b4ef8a2ac287654b3bab3d
Tanmay0215 <tanmaygupta.0215@gmail.com>
2025-03-13 20:45:31 +0530

    updated features

--- src/features.jsx
+++ src/features.jsx
@@ -1,41 +1,38 @@
 import { Book, PencilRuler, Video } from 'lucide-react'
 
 export const features = [
-  {
-    title: 'Vocabulary Builder',
-    description: 'Learn new Japanese words and phrases daily.',
-    icon: Book,
-  },
-  {
-    title: 'Grammar Practice',
-    description: 'Master Japanese grammar with interactive exercises.',
-    icon: PencilRuler,
-  },
-  {
-    title: 'Youtube Subtitle Translator',
-    description: 'Translate Japanese subtitles to your language.',
-    icon: Video,
-  },
+  'Youtube Immersion',
+  'AI Practice',
+  'JoyoKanji + Flash Cards',
 ]
-
 
 export const featureDetails = [
   {
-    "id": 1,
-    "image": "./JoyoKanji.png",
-    "title": "KANJI",
-    "description": "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Temporibus voluptatem adipisci ducimus aperiam, consequatur odio deleniti suscipit distinctio, nemo sunt animi?"
+    id: 1,
+    image: './JoyoKanji.png',
+    title: 'Effortless YouTube Immersion by AI',
+    description:
+      'Enhance your Japanese learning with seamless YouTube immersion using AI ! Our tool lets you hover over any word in subtitles to get instant meanings in context—no more pausing, searching, or breaking your flow. Shadow native speakers with confidence and understand every sentence effortlessly.',
   },
   {
-    "id": 2,
-    "image": "JoyoKanji.png",
-    "title": "KANJI",
-    "description": "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Temporibus voluptatem adipisci ducimus aperiam, consequatur odio deleniti suscipit distinctio, nemo sunt animi?"
+    id: 2,
+    image: 'JoyoKanji.png',
+    title: 'JLPT AI Scenario Practice',
+    description:
+      "Prepare for the JLPT with immersive AI-driven scenario practice! Engage in real-life conversations, get instant corrections, and improve your reading and listening skills with text-to-speech support. Practice like you're talking to a friend—natural, interactive, and effective.",
   },
   {
-    "id": 3,
-    "image": "JoyoKanji.png",
-    "title": "KANJI",
-    "description": "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Temporibus voluptatem adipisci ducimus aperiam, consequatur odio deleniti suscipit distinctio, nemo sunt animi?"
+    id: 3,
+    image: 'JoyoKanji.png',
+    title: 'JoyoKanji Collection - Mnemonics & Flashcards',
+    description:
+      'Memorize all Joyo Kanjis faster with powerful mnemonics and intelligent flashcards! Visualize each kanji with memorable stories, reinforced by spaced repetition for long-term retention. Make kanji learning effortless and fun!',
+  },
+  {
+    id: 4,
+    image: 'JoyoKanji.png',
+    title: 'Track Your  Progress - Stay Motivated & Improve',
+    description:
+      "Stay on top of your learning with detailed progress tracking! See your daily progress and watch your mastery grow over time. With  analytics and goal setting, you'll stay motivated and achieve fluency faster!",
   },
 ]
--- src/pages/Features.jsx
+++ src/pages/Features.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import { features } from '../features'
-import FeatureCard from '../components/FeatureCard'
 import FeatureDetails from '../components/FeatureDetails'
 import { motion } from 'framer-motion'
 import { featureDetails } from '../features'
@@ -8,16 +7,20 @@ import { featureDetails } from '../features'
 const Features = () => {
   return (
     <div className="flex flex-col items-center justify-center">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 max-w-6xl py-2">
+        {features.map((feature, index) => (
+          <div key={index}>
+            <motion.div className="border-2 border-primary/60 rounded-md py-2 px-4 text-center">
+              {feature}
+            </motion.div>
+          </div>
+        ))}
+      </div>
       <div className="text-center py-8">
         <h1 className="text-4xl font-bold text-primary">FEATURES</h1>
         <p className="text-gray-400 mt-2">
           Explore the amazing features of KIKUGO.
         </p>
-      </div>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 p-4 max-w-6xl">
-        {features.map((feature, index) => (
-          <FeatureCard key={index} {...feature} />
-        ))}
       </div>
       <div>
         {featureDetails.map((feature) => (
